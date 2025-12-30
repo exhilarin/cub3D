53b029e673ea211094b546d33ce934b0a4232926
ilyasguney <ilyasguney105@gmail.com>
2025-12-30 06:33:28 +0300

    refactor: fixed render frame functions

--- src/game/mlx.c
+++ src/game/mlx.c
@@ -12,24 +12,28 @@ static void init_mlx(t_game *game)
     game->image = mlx_new_image(game->mlx, LENGHT, HEIGHT);
     if (!game->image)
         return (free_game(game), free(game->win), free(game->mlx));
-    game->addres = mlx_get_data_addr(game->image, &game->img_addr.bpp, &game->img_addr.line_lenght, &game->img_addr.endian);
+    game->addres = mlx_get_data_addr(game->image, &game->img_addr.bpp,
+        &game->img_addr.line_lenght, &game->img_addr.endian);
     if (!game->addres)
         return (free_game(game), free(game->win), free(game->mlx));
 }
 
 static void render_frame(t_game *game)
 {
-    char *pixel_adrr;
-    char *color;
+    char    *pixel_adrr;
+    char    *color;
     for (int y = 0; y < HEIGHT; y++)
     {
         if (y < HEIGHT / 2)
-            color = game->textures.floor_color;
+            color = (char *)&game->textures.floor_color;
         else
-            color = game->textures.ceiling_color;
+            color = (char *)&game->textures.ceiling_color;
         for (int x = 0; x < LENGHT; x++)
+        {
             pixel_adrr = game->addres + (y * game->img_addr.line_lenght + x 
-                    * (game->img_addr.bpp / 8));
+                * (game->img_addr.bpp / 8));
+            *(unsigned int *)pixel_adrr = *(unsigned int *)color;
+        }
     }
     mlx_put_image_to_window(game->mlx, game->win, game->image, 0, 0);
 }
@@ -45,6 +49,7 @@ static void free_mlx(t_game *game)
 void game_loop(t_game *game)
 {
     init_mlx(game);
-    mlx_loop_hook(game->mlx, render_frame, game);
+    mlx_loop_hook(game->mlx, (void *)render_frame, game);
     mlx_loop(game->mlx);
+    free_mlx(game);
 }
